e22e37a3625b6f2671e65f232966af0b2245b023
Suman Saurabh <susaurabh@gmail.com>
2023-12-11 05:19:33 -0500

    Added graphql reference project

---
+++ graphql-jpa-h2/README.md
@@ -0,0 +1,13 @@
+# springboot-graphql-jpa-h2
+
+This is Springboot reference project for graphql implementation with JPA and H2 Database.
+
+## Pre-requisites
+- java 21+
+- maven 3.9.6+
+
+## Build
+Run following command to build and package
+```
+mvn clean test
+```
---
+++ graphql-jpa-h2/src/main/java/com/innodeatech/graphql/Book.java
@@ -0,0 +1,20 @@
+package com.innodeatech.graphql;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Book(Integer id, String name, int pageCount, String authorId) {
+
+    private static List<Book> books = Arrays.asList(
+            new Book(1, "Effective Java", 416, "author-1"),
+            new Book(2, "Hitchhiker's Guide to the Galaxy", 208, "author-2"),
+            new Book(3, "Down Under", 436, "author-3")
+    );
+
+    public static Book getById(Integer id) {
+        return books.stream()
+				.filter(book -> (book.id().equals(id)))
+				.findFirst()
+				.orElse(null);
+    }
+}
